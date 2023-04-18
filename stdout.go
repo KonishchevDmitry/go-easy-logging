328c9a2e1e90b89999454ad2be36295ec433031e
Dmitry Konishchev <konishchev@gmail.com>
2023-04-18 21:37:34 +0300

    Add stdout core

---
+++ stdout.go
@@ -0,0 +1,52 @@
+package logging
+
+import (
+	"os"
+
+	"go.uber.org/zap/zapcore"
+)
+
+type stdoutCore struct {
+	zapcore.LevelEnabler
+	encoder zapcore.Encoder
+}
+
+var _ zapcore.Core = &stdoutCore{}
+
+func newStdoutCore(level zapcore.LevelEnabler, encoder zapcore.Encoder) zapcore.Core {
+	return &stdoutCore{
+		LevelEnabler: level,
+		encoder:      encoder,
+	}
+}
+
+func (c *stdoutCore) With(fields []zapcore.Field) zapcore.Core {
+	return c
+}
+
+func (c *stdoutCore) Check(entry zapcore.Entry, checkedEntry *zapcore.CheckedEntry) *zapcore.CheckedEntry {
+	if c.Enabled(entry.Level) {
+		checkedEntry = checkedEntry.AddCore(entry, c)
+	}
+	return checkedEntry
+}
+
+func (c *stdoutCore) Write(entry zapcore.Entry, fields []zapcore.Field) error {
+	buf, err := c.encoder.EncodeEntry(entry, fields)
+	if err != nil {
+		return err
+	}
+	defer buf.Free()
+
+	file := os.Stdout
+	if entry.Level >= zapcore.WarnLevel {
+		file = os.Stderr
+	}
+
+	_, _ = file.Write(buf.Bytes())
+	return nil
+}
+
+func (c *stdoutCore) Sync() error {
+	return nil
+}
